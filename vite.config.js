bc1979fc17f241a7c1afe8418a1544ebcf131206
heavenlyAura <goodnessokafor462@gmial.com>
2024-07-21 07:05:36 -0700

    "Formatting and whitespace changes in HTML, CSS, and JavaScript files"

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
       },
     },
   },
