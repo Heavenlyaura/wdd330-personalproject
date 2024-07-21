5fbdc4eacccd2b8737342e7800e71b02e04e350d
heavenlyAura <goodnessokafor462@gmial.com>
2024-07-21 04:32:56 -0700

    "Updated index.html with new search form and sections, added helper functions and methods to homeFunc.mjs, and modified main.js and utils.mjs accordingly."

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -3,7 +3,8 @@ const utils = {};
 utils.selectRandomItems = function (numOfItems, list, interval, callback) {
   let selectedItems = [];
   let selectedIndices = new Set();
-  const intervalId = setInterval(() => {
+
+  function selectItems() {
     selectedItems = [];
     selectedIndices.clear();
     while (selectedIndices.size < numOfItems) {
@@ -14,21 +15,42 @@ utils.selectRandomItems = function (numOfItems, list, interval, callback) {
     if (callback && typeof callback === "function") {
       callback(selectedItems);
     }
-  }, interval);
+  }
+  selectItems();
+  const intervalId = setInterval(selectItems, interval);
   return intervalId;
 };
 
-utils.displayImages = (parentElement, arrWithImg, width) => {
+
+utils.displayImages = (parentElement, arrWithImg, width, displayText) => {
   arrWithImg.forEach((item) => {
-    const div = document.createElement('div')
-    const prefix = `https://image.tmdb.org/t/p/w${String(width)}`;
+    const div = document.createElement("div");
+    const prefix = `https://image.tmdb.org/t/p/w${width}`;
+    const aTag = document.createElement("a");
+    aTag.setAttribute("href", `../movie_detail/index.html?${item.id}`);
     const imageElement = document.createElement("img");
     const imageSrc = `${prefix}${item.poster_path}`;
     imageElement.setAttribute("src", imageSrc);
     imageElement.setAttribute("alt", item.original_title);
-    div.appendChild(imageElement)
+    aTag.appendChild(imageElement);
+    div.appendChild(aTag);
     parentElement.appendChild(div);
+    if (displayText === "info") {
+      displayMovieLinks(parentElement, item);
+    }
   });
 };
+
+function displayMovieLinks(parentElement, movie) {
+  const aTag = document.createElement("a");
+  aTag.setAttribute("href", `../movie_detail/index.html?${movie.id}`);
+  const pTag = document.createElement("p");
+  const pTag2 = document.createElement("p");
+  pTag.textContent = `Movie: ${movie.title};`;
+  pTag2.textContent = `Release Date: ${movie.release_date}`;
+  aTag.appendChild(pTag);
+  aTag.appendChild(pTag2);
+  parentElement.appendChild(aTag);
+}
 
 export { utils };
